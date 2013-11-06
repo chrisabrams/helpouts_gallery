1f885f74e7d323999140b9151c2a55354e83c65e
Chris Abrams <mail@chrisabrams.com>
2013-11-06 17:38:47 -0500

    Add photo collection.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+var Photo = Backbone.Model.extend({
 
+  defaults: {
+    photoUrl: '/img/'
+  },
+
+  initialize: function() {
+
+  }
+
+});
+
+var Album = Backbone.Collection.extend({
+
+  model: Photo,
+
+  initialize: function() {
+
+  }
+
+});
+
+
+var album = new Album([
+
+  { photoUrl: '/img/01.jpg' },
+  { photoUrl: '/img/02.jpg' },
+  { photoUrl: '/img/03.jpg' },
+  { photoUrl: '/img/04.jpg' },
+  { photoUrl: '/img/05.jpg' },
+  { photoUrl: '/img/06.jpg' },
+  { photoUrl: '/img/07.jpg' },
+  { photoUrl: '/img/08.jpg' }
+
+]);
